baf0a2dd8920cc8ae36580b0bbc8bf822d31b103
codenamerhu <rndhlovu@meetdchef.com>
2021-02-13 17:25:21 +0200

    ui build

---
+++ Stackoverflow Search/Presentation/views/Search Table/SearchResultTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResultTableViewCell.swift
+//  Stackoverflow Search
+//
+//  Created by Rhulani Ndhlovu on 2021/02/13.
+//
+
+import UIKit
+
+class SearchResultTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Stackoverflow Search/Presentation/views/SearchViewViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchViewViewController.swift
+//  Stackoverflow Search
+//
+//  Created by Rhulani Ndhlovu on 2021/02/13.
+//
+
+import UIKit
+
+class SearchViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
